c3e99445fd2b3a8ddfab145a2c27ad6d7b5c21e6
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2016-03-09 00:10:00 -0500

    added install script

--- mc/sched.py
+++ mc/sched.py
@@ -1,4 +1,3 @@
 """
 Machine Colony scheduler service interface.
-Copyright 2016 Machine Colony Inc. All rights reserved.
 """
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import sys
+from setuptools import setup, find_packages
+
+if sys.argv[-1] == 'install':
+    setup(name='pycolony',
+          version='0.0.1',
+          description='Python Machine Colony SDK',
+          author='Machine Colony Inc.',
+          author_email='maxwell@machinecolony.com',
+          url='https://github.com/MachineColony/pyColony',
+          packages=find_packages()
+          )
