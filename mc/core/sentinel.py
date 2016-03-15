59364faf4e1b2e5bd244b0c87d924222afabbf88
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2016-03-15 09:26:43 -0400

    email interface, update JSON POSTs

--- mc/core/sentinel.py
+++ mc/core/sentinel.py
@@ -1,12 +1,13 @@
 # Interface to the Sentinel API for anomaly detection
+import json
 import requests
 
 
 def evaluate(data, model_id):
-    _data = {'data': data}
+    _data = json.dumps({'data': data})
     return requests.post("https://sentinel.machinecolony.com/models/%s/evaluate" % model_id, data=_data)
 
 
 def create_model(data):
-    _data = {'data': data}
+    _data = json.dumps({'data': data})
     return requests.post("https://sentinel.machinecolony.com/create", data=_data)
