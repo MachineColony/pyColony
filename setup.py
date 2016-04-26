640e3a0ac1f4b957fd74f43128b88eb1184b50cf
Maxwell Rebo <maxwell.b.rebo@gmail.com>
2016-04-26 13:26:58 -0400

    version bump to 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 if sys.argv[-1] == 'install':
     setup(name='pycolony',
-          version='0.0.15',
+          version='0.1.0',
           description='Python Machine Colony SDK',
           author='Machine Colony Inc.',
           author_email='maxwell@machinecolony.com',
