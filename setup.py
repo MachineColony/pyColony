2885a465f74f3c73d626c73df004516d8a4257ee
Francis Tseng <f+accounts@frnsys.com>
2016-05-27 09:48:14 -0400

    included requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-
-import os
 import sys
 from setuptools import setup, find_packages
 
@@ -11,5 +8,7 @@ if sys.argv[-1] == 'install':
           author='Machine Colony Inc.',
           author_email='maxwell@machinecolony.com',
           url='https://github.com/MachineColony/pyColony',
-          packages=find_packages()
-          )
+          packages=find_packages(),
+          install_requires=[
+            'requests'
+          ])
